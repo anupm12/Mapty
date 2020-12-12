0a9c810217dca66ca46b0981e742f43e2f0ab3c7
anupm12 <shuklaanupam18@gmail.com>
2020-12-12 14:53:57 +0530

    Popup styling✔

--- script.js
+++ script.js
@@ -24,19 +24,29 @@ if (navigator.geolocation)
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
+
       L.marker(coord)
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
-    },
 
-    map.on('click', function (mapEvent) {
+      map.on('click', function (mapEvent) {
         const { lat, lng } = mapEvent.latlng;
         L.marker([lat, lng])
           .addTo(map)
-          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
           .openPopup();
       });
+    },
 
     function () {
       alert('Could not get location');
