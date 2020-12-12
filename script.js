96e456c4c290898a694300a3acd3c682506d35d8
anupm12 <shuklaanupam18@gmail.com>
2020-12-12 14:37:31 +0530

    Display map using leaflet library

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { longitude } = position.coords;
+      const { latitude } = position.coords;
+      const coord = [latitude, longitude];
+
+      //   leaflet
+      var map = L.map('map').setView([latitude, longitude], 13);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+      L.marker(coord)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+
+    function () {
+      alert('Could not get location');
+    }
+  );
