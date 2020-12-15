6001d9871e0703706a6afb4e53771c9c6426f528
anupm12 <shuklaanupam18@gmail.com>
2020-12-16 01:17:29 +0530

    Validating input

--- script.js
+++ script.js
@@ -3,6 +3,43 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -59,10 +96,39 @@ class App {
   }
 
   _newWorkout(e) {
-    e.preventDefault();
-    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value =
-      '';
+    const validInputs = (...inputs) => inputs.every(inp => isFinite(inp));
 
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    e.preventDefault();
+
+    // get the data
+    const type = inputType.value;
+    const distance = inputDistance.value;
+    const duration = inputDuration.value;
+
+    // check if data is valid
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      ) {
+        return alert('Inputs have to be positive numbers!!');
+      }
+    }
+
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      ) {
+        return alert('Inputs have to be positive numbers!!');
+      }
+    }
+
+    // render marker
     const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
@@ -77,6 +143,9 @@ class App {
       )
       .setPopupContent('Workout')
       .openPopup();
+
+    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value =
+      '';
   }
 }
 
